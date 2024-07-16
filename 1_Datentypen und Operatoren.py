a3b29fe37dd05f35df27ffa14dd67663b40b6fc7
Stefan Koschnik <stefan.koschnik@karrieretutor.de>
2024-07-16 07:36:59 +0200

    Update 1_Datentypen und Operatoren.py
    Binäroperationen erläutert

--- 1_Datentypen und Operatoren.py
+++ 1_Datentypen und Operatoren.py
@@ -46,7 +46,8 @@ Zuweisungsoperatoren:
 *=
 /=
 **=     b **= 10 (b hoch 10)
-//=     (wenn a = 10) a //= 4 (a == 2)
+//=     (wenn a = 10) a //= 4 -> a == 2
+|=
 
 
 Logikoperatoren:
@@ -56,7 +57,7 @@ and, or, not
 
 Bitweise Operatoren:
 
-- &, |, ^, ~, <<, >>
+&, |, ^, ~, <<, >>
 
 
 Membership- Operatoren
@@ -120,6 +121,7 @@ print("Der Titel '"+var7+"' enthält",len(var7.split()),"Worte.") # Konkatinieru
 print("Der Titel '"+var7+"' enthält "+str(len(var7.split()))+" Worte.") # Durch Konvertierung zu String (str(len(var7.split()))) Konkatination möglich
 print(f"Der Titel '{var7}' enthält {len(var7.split())} Worte.")
 print("Der Titel '{}' enthält {} Worte.".format(var7, len(var7.split())))
+
 """
 Vorsicht! Das Folgende funktioniert nicht:
 
@@ -134,8 +136,138 @@ IndexError: Replacement index 1 out of range for positional args tuple
 Das meint, dass der zweite Platzhalter (Replacement index 1) in .format() nicht übergeben wurde.
 """
 
+# Abprüfen von zwei Konditionen
+print("\nAbprüfen von zwei Konditionen")
+print("10<5 and 4<5 = ", 10<5 and 4<5) # Beide Konditionen müssen zutreffen, damit 
+                    # der Gesamtausdruck WAHR wird, daher hier 
+                    # Gesamtausdruck FALSCH
+
+print("10<5 or 4<5 = ",10<5 or 4<5)  # Nur eine der beiden Konditionen muss zutreffen,
+                    # damit der Gesamtausdruck WAHR wird, daher hier 
+                    # Gesamtausdruck WAHR
+"""
+
+11.07.2024 -> Little Endian (less significant bit on the left side)
+2024/07/11 -> Big Endian
+
+2^7 2^6 2^5 2^4 2^3 2^2 2^1 2^0     Binärinterpretation bei Big Endian
+
+Beim verODERn wird im Ergebnis das Bit auf 1 gesetzt,
+wenn es in mindestens einer der beiden Ausgangsreihen auf 1 war
+
+0000 0001   = 1
+0000 0010   = 2
+_______________ | (bitweises or)
+0000 0011   = 3
 
 
+Beim verUNDen wird im Ergebnis das Bit auf 1 gesetzt,
+wenn es in beiden Ausgangsreihen auf 1 war
 
+0000 0001   = 1
+0000 0010   = 2
+_______________ & (bitweises und)
+0000 0000   = 0
+
+aber: 
+
+0000 0001   = 1
+0000 0011   = 3
+_______________ & (bitweises und)
+0000 0001   = 1
+
+
+Beim verXODERn wird im Ergebnis das Bit auf 1 gesetzt,
+wenn es nur(!) in einer der beiden Ausgangsreihen auf 1 war
+
+0000 1011   = 11
+0000 1001   = 9
+_______________^ (bitweises xor, exklusives oder)
+0000 0010   = 2
+
+
+verNOTen lässt sich immer nur ein Wert. Dabei werden die Bit-setzungen
+einfach umgekehrt 
+
+0000 0011   = 3
+_______________ ~
+1111 1100   = 252
+
+ABER SIEHE ZEILE 252, reale VerNOTung führt zur Bildung des Zweierkomplements. 
+"""
+# Verschiebeoperatoren >> , << 
+
+print("\nVerschiebeoperatoren >> , <<")
+var9 = 2    # 0000 0010
+print("var9:",var9," binär:",bin(var9))
+
+# Linksverschiebung um eine Stelle verdoppelt den Wert
+var9<<=1    # 0000 0100 Binärreihe aus Zeile 201 wird um eine Stelle nach links verschoben
+print("var9 nach Linksverschiebung (eine Stelle):",var9, " binär:",bin(var9))
+
+# Rechtsverschiebung um eine Stelle halbiert den Wert
+var9>>=2    # 0000 0001 Verschiebung um 2 Stellen nach rechts
+print("var9 nach Rechtsverschiebung (2 Stellen):",var9, " binär:",bin(var9))
+
+print("\nDurch Verschiebung rausfallende Bits können nicht wiederhergestellt werden.")
+var10 = 3    # 0000 0011
+print(("var10: ", var10, " binär:",bin(var10)))
+var10 >>= 2  # 0000 0000
+print("var10 nach zweifacher Rechtsverschiebung: ", var10, " binär:",bin(var10))
+var10<<=2
+print("var10 nach anschließender zweifacher Linksverschiebung: ", var10, " binär:",bin(var10))
+# Durch Rechts- Verschiebung rausfallende positive Bits
+# werden durch anschließende Links- Verschiebung nicht
+# wiederhergestellt
+
+
+# Bitweises OR
+print("\nBitweises OR")
+var11 = 4    # 0000 0100
+print("var11:",var11, "binär: ", bin(var11))
+print("Veroderung von var11(=4 bzw. 0100) mit 2 (0010)")
+var11 |= 2   # 0000 0010
+# 0000 0110
+print("var11 nach VerODERung: ",var11," bzw.", bin(var11)) # var11 ist jetzt 6
+
+# Bitweises AND
+print("\nBitweises AND")
+var12 = 4    # 0000 0100
+print("var12:", var12, " bzw.", bin(var12))
+var12 &= 2   # 0000 0010
+# 0000 0000
+print("var12 nach VerUNDung mit 2 (0010): ",var12, " bzw. ", bin(var12)) # var12 ist jetzt 0
+
+# Bitweises XOR
+print("\nBitweises XOR")
+var13 = 5    # 0000 0101
+print("var13:", var13, " bzw.", bin(var13))
+var13 ^= 4   # 0000 0100
+# 0000 0001
+print("var9 nach VerXODERung mit 4 (0100): ",var13," bzw.", bin(var13)) # var13 ist jetzt 1
+
+print("\nVerNOTen von 3 (siehe Zeile 192)")
+print(~(0b11), bin(~(0b11)))
+"""
+Ausgabe: -4 -0b100 statt 252 0b11111100
+
+Python wertet den Ausdruck ~0b00000011 als Zweierkomplement aus
+Somit wird daraus nicht 252 sondern -4. 
+
+siehe: Bildung eines Zweierkomplements bei NOT in Python
+
+https://www.youtube.com/watch?v=__-OEo6CHQ8
+
+Kurze Regel:
+
+Ausgangspunkt positive Zahl:
+
+Addiert 1 zu absoluten Wert und kehrt das Vorzeichen um zu Minus
+
+Ausgangspunkt negative Zahl:
+
+Substrahiert 1 vom absoluten Wert und kehrt das Vorzeichen um zu Plus
+
+"""
 
 
